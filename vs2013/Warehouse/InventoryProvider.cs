1ab77270f485cd22272f0e5976cd7b8712ad7ca7
Erlend Rotvik <erlend@rotvik.net>
2015-10-31 17:05:19 +0100

    Added warehouse service

---
+++ vs2013/Warehouse/InventoryProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Warehouse
+{
+    public class InventoryProvider
+    {
+        private static Inventory[] _inventories;
+
+        private static IEnumerable<Inventory> Inventories
+        {
+            get
+            {
+                return _inventories ?? (_inventories = JsonConvert.DeserializeObject<Inventory[]>(File.ReadAllText("inventory.json")));
+            }
+        }
+
+        public Inventory FindById(string id)
+        {
+            return Inventories.FirstOrDefault(x => x.BookId == id);
+        }
+    }
+}
