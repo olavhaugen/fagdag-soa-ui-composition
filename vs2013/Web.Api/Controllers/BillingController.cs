ccd1af4c2d63ea7aa9f9c31baf71d3199e1e3eca
Oystein Bjorke <oystein.bjorke@gmail.com>
2015-11-01 09:25:43 +0100

    Make billing controller less stable

--- vs2013/Web.Api/Controllers/BillingController.cs
+++ vs2013/Web.Api/Controllers/BillingController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using Billing;
 
 namespace Web.Api.Controllers
@@ -7,13 +8,26 @@ namespace Web.Api.Controllers
     public class BillingController : ApiController
     {
         BillingDetailsStore _detailsStore = new BillingDetailsStore();
+        private Random _random = new Random();
 
         [Route("orders/{id}"), HttpPost]
         public IHttpActionResult PostBillingDetails(string id, string creditCardNumber)
         {
-            var d = new BillingDetails { OrderId = id, CreditCardNumber = creditCardNumber};
+            if (_random.Next(4) == 0)
+            {
+                return InternalServerError(new BillingException("No funding."));
+            }
+
+            var d = new BillingDetails { OrderId = id, CreditCardNumber = creditCardNumber };
             _detailsStore.Add(d);
             return Created("/billing/orders/" + id, d);
+        }
+
+        public class BillingException : Exception
+        {
+            public BillingException(string m) : base(m)
+            {
+            }
         }
     }
 }
