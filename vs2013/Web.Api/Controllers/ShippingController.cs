a11d57fd380487433993b78d13a399bda8c8b597
Oystein Bjorke <oystein.bjorke@gmail.com>
2015-11-01 09:04:18 +0100

    Change shipping route

--- vs2013/Web.Api/Controllers/ShippingController.cs
+++ vs2013/Web.Api/Controllers/ShippingController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Shipping;
 
 namespace Web.Api.Controllers
@@ -9,20 +8,12 @@ namespace Web.Api.Controllers
     {
         ShippingDetailsStore _detailsStore = new ShippingDetailsStore();
 
-        [Route("shippingdetails/{id}"), HttpPost]
+        [Route("orders/{id}"), HttpPost]
         public ShippingDetails PostShippingDetails(string id, string address)
         {
             var d = new ShippingDetails { OrderId = id, Address = address };
             _detailsStore.Add(d);
             return d; // TODO: return code 201?
-        }
-
-        [Route("payments/{id}"), HttpPost]
-        public ShippingDetails PostPaymentReceived(string id)
-        {
-            var sd = _detailsStore.FindById(id);
-            Console.WriteLine("Shipping order {0} to {1}", sd.OrderId, sd.Address);
-            return sd;
         }
     }
 }
